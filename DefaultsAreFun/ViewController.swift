5136e97c3e682a892b2c217e02dff4f9a2f71b04
Emanuel  Guerrero <Emanuel@Emanuels-MBP.attlocal.net>
2016-03-09 21:48:45 -0500

    Added IBAction outlets to the buttons for handling when the user taps on one of the button. Also when the app starts, checks to see if there is a saved value.

--- DefaultsAreFun/ViewController.swift
+++ DefaultsAreFun/ViewController.swift
@@ -9,10 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var favoriteLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        if let color = NSUserDefaults.standardUserDefaults().valueForKey("color") as? String {
+            favoriteLabel.text = "Favorite Color: \(color)"
+        } else {
+            favoriteLabel.text = "Pick a color!"
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func redTapped(sender: UIButton) {
+        favoriteLabel.text = "Favorite Color: Red"
+        NSUserDefaults.standardUserDefaults().setValue("Red", forKey: "color")
+        NSUserDefaults.standardUserDefaults().synchronize()
+    }
+    
+    @IBAction func yellowTapped(sender: UIButton) {
+        favoriteLabel.text = "Favorite Color: Yellow"
+        NSUserDefaults.standardUserDefaults().setValue("Yellow", forKey: "color")
+        NSUserDefaults.standardUserDefaults().synchronize()
+    }
+    
+    @IBAction func blueTapped(sender: UIButton) {
+        favoriteLabel.text = "Favorite Color: Blue"
+        NSUserDefaults.standardUserDefaults().setValue("Blue", forKey: "color")
+        NSUserDefaults.standardUserDefaults().synchronize()
+    }
 }
 
