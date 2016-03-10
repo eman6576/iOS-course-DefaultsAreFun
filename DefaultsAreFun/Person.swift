2704b2cd1be704ab28271ceb504950d0045425de
Emanuel  Guerrero <Emanuel@Emanuels-MBP.attlocal.net>
2016-03-09 22:22:36 -0500

    Made the class inherit from NSObject and implement the NSCoding protocal for endoing and decoding Person objects into NSData objects for loading and saving to the disk.

--- DefaultsAreFun/Person.swift
+++ DefaultsAreFun/Person.swift
@@ -17,5 +17,18 @@ class Person: NSObject, NSCoding {
         self.lastName = last
     }
     
+    override init() {
+        
+    }
     
+    required convenience init?(coder aDecoder: NSCoder) {
+        self.init()
+        self.firstName = aDecoder.decodeObjectForKey("firstName") as? String
+        self.lastName = aDecoder.decodeObjectForKey("lastName") as? String
+    }
+    
+    func encodeWithCoder(aCoder: NSCoder) {
+        aCoder.encodeObject(self.firstName, forKey: "firstName")
+        aCoder.encodeObject(self.lastName, forKey: "lastName")
+    }
 }
